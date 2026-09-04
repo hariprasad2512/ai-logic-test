8423b4b434c81112750191bc3bc9985a13afafa5
hariprasad2512 <hprasad887@gmail.com>
2026-09-04 12:50:37 +0530

    Finish Container with Most Water Code Implementation

--- 4container_with_most_water.py
+++ 4container_with_most_water.py
@@ -6,8 +6,6 @@
 
 # height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
 
- 
-
 # Output:
 
 # 49
@@ -17,4 +15,21 @@
 # ●       Explain your approach and complexity.
 
 def container_with_most_water(height):
-    pass
+    left = 0
+    right = len(height) - 1
+    max_water = 0
+    
+    while left < right:
+        current_height = min(height[left], height[right])
+        current_width = right - left
+        area = current_height * current_width
+        max_water = max(max_water , area)
+        
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+            
+    return max_water
+
+print(container_with_most_water([1, 8, 6, 2, 5, 4, 8, 3, 7]))
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Test Questions from the Technical Round of AI LOGIC Interview
 
 ### Please check the solutions of the problems asked.
+
+1. Two Sum
+2. Majority Element
+3. Maximum Swap
+4. Container with Most Water
